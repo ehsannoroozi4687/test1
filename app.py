1b2cc4f2fcd28e64a91a58853cd86a5897806844
behnam hasanbeygi <b.hasanbeygi@fanap.ir>
2023-03-22 09:49:17 +0330

    first commit.Rest Api with auth and create items

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+
+import create_tables
+
+app = Flask(__name__)
+app.secret_key = 'ehsan'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+
+
+
+if __name__ == "__main__":
+    app.run(port=21000,host="0.0.0.0",debug=True)
